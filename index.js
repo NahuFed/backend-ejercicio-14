6dee40b33b02a0204aaa4c3f204a1b3e6fb32381
Nahuel Rodriguez <nahufed@gmail.com>
2023-07-10 17:04:25 -0300

    se realizo la conexion a la base de datos

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
-import path from "path"
+import path from 'path'
+import 'dotenv/config'
+import './src/database/dbConnection'
 
 const app = express();
 
-app.set('port',  process.env.PORT || 4000);
+app.set('port',  process.env.PORT || 4001);
 app.listen(app.get('port'),()=>{
     console.log('Estoy en el puerto ' +app.get('port'))
 });
@@ -14,6 +16,5 @@ app.listen(app.get('port'),()=>{
 app.use(cors());
 app.use(express.json());
 app.use(morgan('dev'));
-console.log(path.join(__dirname,'/public'));
-app.use(express.static(path.join(__dirname,'/public')))
-//http://localhost:4000
+console.log(path.join(__dirname, '/public'))
+app.use(express.static(path.join(__dirname, '/public')))
