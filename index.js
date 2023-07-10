bcc1a379f3a995930f5243fd85a5e98d635dd20f
Nahuel Rodriguez <nahufed@gmail.com>
2023-07-10 15:02:06 -0300

    se termino la configuracion inicial

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import cors from 'cors';
+import morgan from 'morgan';
+import path from "path"
 
 const app = express();
 
@@ -11,4 +13,7 @@ app.listen(app.get('port'),()=>{
 
 app.use(cors());
 app.use(express.json());
-
+app.use(morgan('dev'));
+console.log(path.join(__dirname,'/public'));
+app.use(express.static(path.join(__dirname,'/public')))
+//http://localhost:4000
